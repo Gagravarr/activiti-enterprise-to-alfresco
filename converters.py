16f406924f252b70a377024e43fe08d9b0504406
Nick Burch <nick@gagravarr.org>
2016-06-03 15:18:15 +0100

    XML escape more user-supplied text on output

--- converters.py
+++ converters.py
@@ -153,10 +153,10 @@ class ModelOutput(Output):
       if alf_type:
          self.write("         <property name=\"%s\">\n" % alf_id)
          if name:
-            self.write("           <title>%s</title>\n" % name)
+            self.write("           <title>%s</title>\n" % escape(name))
          self.write("           <type>%s</type>\n" % alf_type)
          if ftype == "readonly-text":
-            self.write("           <default>%s</default>\n" % field.get("value",""))
+            self.write("           <default>%s</default>\n" % escape( field.get("value","") ))
          if required:
             self.write("           <mandatory>true</mandatory>\n")
          if options:
@@ -164,7 +164,7 @@ class ModelOutput(Output):
             self.write("             <constraint type=\"LIST\">\n")
             self.write("               <parameter name=\"allowedValues\"><list>\n")
             for opt in options:
-               self.write("                 <value>%s</value>\n" % opt["name"])
+               self.write("                 <value>%s</value>\n" % escape(opt["name"]))
             self.write("               </list></parameter>\n")
             self.write("             </constraint>\n")
             self.write("           </constraints>\n")
